6a022c8a23621a4fe89d556b6e46fe45e5600ca5
Amina farha <aminafarha1510@gmail.com>
2023-11-12 14:13:43 +0530

    test file dltd

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'basic_calcu.dart';
 void main() {
   runApp(const MaterialApp(
     home: simpleCalcu(),
-    debugShowCheckedModeBanner: false,
+    debugShowCheckedModeBanner: true,
   ));
 }
